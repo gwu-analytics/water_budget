5e599e83278881e52965195a7b472cd403f0b377
gregorydukecog <161615320+gregorydukecog@users.noreply.github.com>
2024-05-24 15:38:56 -0500

    Updated files, added data file format

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from tkinter import filedialog
 import yaml
 from mdm_query import *
 import datetime
+import openpyxl
 
 
 class Customer:
-    def __init__(self, name, acreage):
-        self.name, self.acreage, self.allowance, self.meters, self.usage = name, acreage, None, None, None
+    def __init__(self, name, footage):
+        self.name, self.footage, self.allowance, self.meters = name, footage, None, None
         self.mon_viol, self.mid_viol, self.bug_viol = None, None, None
+        self.water, self.irrigation = [], []
         self.__acc_party = None
 
     def set_acc_party(self, acc_party):
@@ -20,9 +22,6 @@ class Customer:
     def set_meters(self, meters):
         self.meters = meters
 
-    def set_usage(self, usage):
-        self.usage = usage
-
     def set_viols(self, mon, mid, bug):
         self.mon_viol, self.mid_viol, self.bug_viol = mon, mid, bug
 
@@ -30,8 +29,16 @@ class Customer:
         return self.__acc_party
 
 
-def calculate_budget(acres):
-    return (acres * 0.83 * 7.48) / 1000
+class Meter:
+    def __init__(self):
+        self.type, self.data = None, None
+
+    def set_meters(self, meter_type, meter_data):
+        self.type, self.data = meter_type, meter_data
+
+
+def calculate_budget(acres, dcp):
+    return (acres * 0.83 * 7.48 * dcp) / 1000
 
 
 def file_explorer():
@@ -42,15 +49,14 @@ def file_explorer():
 
 if __name__ == "__main__":
 
-    print('='*50)
+    print('=' * 50)
     print('Master-Metered Community Violation Application\nWater Analytics 2024')
-    print('='*50+'\n- Select your data file.\n')
+    print('=' * 50 + '\n- Select your data file.\n')
 
-    file_selected = file_explorer()  # needs error handling
-    with open(file_selected, 'r') as file:
-        data = yaml.safe_load(file)
+    file_selected = file_explorer()
+    data = pd.read_excel(file_selected)
 
-    while True: # needs date conversion to past monday or something
+    while True:  # needs date conversion to past monday or something
         try:
             date = input('- Enter target date like YYYY-MM-DD: ')
             datetime.date.fromisoformat(date)
@@ -62,7 +68,7 @@ if __name__ == "__main__":
     while cust_choice != '1' and cust_choice != '2':
         cust_choice = input(' > Enter only a 1 or 2, please:')
     if cust_choice == '2':
-        cust_choice = input('- Enter the account number for your customer: ') # needs error handling
+        cust_choice = input('- Enter the account number for your customer: ')  # needs error handling
 
     dcp = int(input('- Enter the current DCP stage, from 0 to 4: '))
     while dcp not in range(0, 4):
@@ -71,17 +77,19 @@ if __name__ == "__main__":
     print('Thank you! Please hold while we generate your data.')
 
     customers = []
-    for i in data:
-        customer = Customer(name=i['customerName'], acreage=i['irrigatableArea'])
-        customer.set_acc_party(acc_party=i['customerNumber'])
-        customer.set_allowance(allowance=calculate_budget(i['irrigatableArea']))
-        customer.set_meters(meters=i['meters'])
-        #customer.set_usage(usage=query_mdm_intervals(customer.get_acc_party(), date))
+    for row in data.itertuples(index=True, name='Customer'):
+        customer = Customer(name=row.CustomerName, footage=row.IrrigatableArea)
+        customer.set_acc_party(acc_party=row.CustomerNumber)
+        customer.set_allowance(allowance=calculate_budget(row.IrrigatableArea, dcp))
+        
+
         # Querying usage data and returning hourly reads
+
+
         # Parsing reads and counting number of violations by type
         customers.append(customer)
 
-    print('='*50)
+    print('=' * 50)
     for customer in customers:
         print('Generated data for', customer.name)
         # Excel output
